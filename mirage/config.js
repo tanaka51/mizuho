3b6cbe4a7ec9109be3e0c91f9bd47d157c42c5a4
Koichi Tanaka <tanaka51.jp@gmail.com>
2016-08-24 03:37:56 +0900

    Fake games

--- mirage/config.js
+++ mirage/config.js
@@ -1,26 +1,50 @@
 export default function() {
-
-  // These comments are here to help you get started. Feel free to delete them.
-
-  /*
-    Config (with defaults).
-
-    Note: these only affect routes defined *after* them!
-  */
-
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-
-  /*
-    Shorthand cheatsheet:
-
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-
-    http://www.ember-cli-mirage.com/docs/v0.2.x/shorthands/
-  */
+  this.get('games', function () {
+    return {
+      data: [{
+        type: 'games',
+        id: '1',
+        relationships: {
+          users: {
+            data: [
+              { type: 'users', id: '1' },
+              { type: 'users', id: '2' },
+              { type: 'users', id: '3' },
+              { type: 'users', id: '4' }
+            ]
+          }
+        }
+      }, {
+        type: 'games',
+        id: '2',
+        relationships: {
+          users: {
+            data: [
+              { type: 'users', id: '4' },
+              { type: 'users', id: '1' },
+              { type: 'users', id: '3' },
+              { type: 'users', id: '2' }
+            ]
+          }
+        }
+      }],
+      included: [{
+        type: 'users',
+        id: '1',
+        attributes: { name: 'tanaka51' }
+      }, {
+        type: 'users',
+        id: '2',
+        attributes: { name: 'flada_auxv' }
+      }, {
+        type: 'users',
+        id: '3',
+        attributes: { name: 'mtsmfm' }
+      }, {
+        type: 'users',
+        id: '4',
+        attributes: { name: 'ihei223' }
+      }]
+    };
+  });
 }
