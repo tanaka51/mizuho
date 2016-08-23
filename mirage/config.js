ac320e710030c6562dd09cdba76c0aa9a5f3d0b7
Koichi Tanaka <tanaka51.jp@gmail.com>
2016-08-24 04:11:30 +0900

    Add show page

--- mirage/config.js
+++ mirage/config.js
@@ -1,50 +1,70 @@
+const game1 = {
+  type: 'games',
+  id: '1',
+  relationships: {
+    users: {
+      data: [
+      { type: 'users', id: '1' },
+      { type: 'users', id: '2' },
+      { type: 'users', id: '3' },
+      { type: 'users', id: '4' }
+      ]
+    }
+  }
+};
+
+const game2 = {
+  type: 'games',
+  id: '2',
+  relationships: {
+    users: {
+      data: [
+      { type: 'users', id: '4' },
+      { type: 'users', id: '1' },
+      { type: 'users', id: '3' },
+      { type: 'users', id: '2' }
+      ]
+    }
+  }
+};
+
+const includedUsers = [{
+  type: 'users',
+  id: '1',
+  attributes: { name: 'tanaka51' }
+}, {
+  type: 'users',
+  id: '2',
+  attributes: { name: 'flada_auxv' }
+}, {
+  type: 'users',
+  id: '3',
+  attributes: { name: 'mtsmfm' }
+}, {
+  type: 'users',
+  id: '4',
+  attributes: { name: 'ihei223' }
+}];
+
 export default function() {
   this.get('games', function () {
     return {
-      data: [{
-        type: 'games',
-        id: '1',
-        relationships: {
-          users: {
-            data: [
-              { type: 'users', id: '1' },
-              { type: 'users', id: '2' },
-              { type: 'users', id: '3' },
-              { type: 'users', id: '4' }
-            ]
-          }
-        }
-      }, {
-        type: 'games',
-        id: '2',
-        relationships: {
-          users: {
-            data: [
-              { type: 'users', id: '4' },
-              { type: 'users', id: '1' },
-              { type: 'users', id: '3' },
-              { type: 'users', id: '2' }
-            ]
-          }
-        }
-      }],
-      included: [{
-        type: 'users',
-        id: '1',
-        attributes: { name: 'tanaka51' }
-      }, {
-        type: 'users',
-        id: '2',
-        attributes: { name: 'flada_auxv' }
-      }, {
-        type: 'users',
-        id: '3',
-        attributes: { name: 'mtsmfm' }
-      }, {
-        type: 'users',
-        id: '4',
-        attributes: { name: 'ihei223' }
-      }]
+      data: [game1, game2],
+      included: includedUsers
     };
   });
+
+  this.get('games/1', function () {
+    return {
+      data: game1,
+      included: includedUsers
+    }
+  })
+
+  this.get('games/2', function () {
+    return {
+      data: game2,
+      included: includedUsers
+    }
+  })
 }
